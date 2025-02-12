51872f36d42b3ae81a0b2df47b2ade67a1fb8985
carter <carterbesparza28@gmail.com>
2025-02-11 23:32:44 -0800

    missed an import os

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,9 +1,10 @@
 # Segment 3: backend/app/db.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Because we'll reference this container by the Docker service name "db"
-DATABASE_URL = "postgresql://postgres:postgres@host.docker.internal:5432/analytics_dashboard"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/mydb")
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
